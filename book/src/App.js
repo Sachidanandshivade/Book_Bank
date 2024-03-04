04b7572c78592eb65764a873173a9a22946a32e1
sachidanand <114554207+Sachidanandshivade@users.noreply.github.com>
2024-03-04 17:16:34 +0000

    new

--- book/src/App.js
+++ book/src/App.js
@@ -1,13 +1,21 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import BookCreate from './components/BookCreate';
-
+import BookList from './components/BookList';   
 function App() {
-  const [books,setBooks]=useState([]);
-  const createBook =(title) =>{
-    console.log('need to add bookk with:',title);
-  }
-  return <div><BookCreate onCreate={createBook}/></div>;
+  const [books, setBooks] = useState([]);
 
+  const createBook = (title) => {
+    const updatedBooks = [...books, { id: Math.round(Math.random(),999), title }];
+    setBooks(updatedBooks);
+  };
+
+  return (
+    <div className="app">
+      <BookList books={books}/>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
+
